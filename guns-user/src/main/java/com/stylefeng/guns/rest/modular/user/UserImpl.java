d6a907f4ce5ce20b02796bfea13cafacbaa5da5b
liuenci <154910381@qq.com>
2019-09-08 22:29:51 +0800

    :sparkles: 引入用户模块

---
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
@@ -0,0 +1,15 @@
+package com.stylefeng.guns.rest.modular.user;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.stylefeng.guns.api.user.UserAPI;
+import org.springframework.stereotype.Component;
+
+@Component
+@Service(interfaceClass = UserAPI.class)
+public class UserImpl implements UserAPI{
+    @Override
+    public boolean login(String username, String password) {
+        System.out.println("this is user service!! " + username + " " + password);
+        return false;
+    }
+}
