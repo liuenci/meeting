c9bd416e530d37d16858d0c2c70aa226dc9106dd
liuenci <liuencier@gmail.com>
2019-09-14 13:08:44 +0800

    :construction: ThreadLocal 保存用户信息

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/common/CurrentUser.java
@@ -0,0 +1,32 @@
+package com.stylefeng.guns.rest.common;
+
+import com.stylefeng.guns.api.user.UserInfoModel;
+
+public class CurrentUser {
+    /**
+     * 线程绑定的存储空间
+      */
+    private static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    public static void saveUserId(String userId){
+        threadLocal.set(userId);
+    }
+    public static String getCurrentUser() {
+        return threadLocal.get();
+    }
+
+//    /**
+//     * 将用户信息放入存储空间
+//     * @param userInfoModel
+//     */
+//    public static void saveUserInfo(UserInfoModel userInfoModel){
+//        threadLocal.set(userInfoModel);
+//    }
+//
+//    /**
+//     * 将用户信息取出
+//     * @return
+//     */
+//    public static UserInfoModel getCurrentUser(){
+//        return threadLocal.get();
+//    }
+}
