c53f83a521b807c3b3740b2c41fc1c1c8c055823
liuenci <154910381@qq.com>
2019-09-14 17:42:36 +0800

    :construction: 注册业务实现

---
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package com.stylefeng.guns.rest.modular.user;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocUserTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Service(interfaceClass = UserAPI.class)
+public class UserServiceImpl implements UserAPI{
+    @Autowired
+    private MoocUserTMapper moocUserTMapper;
+
+    @Override
+    public int login(String username, String password) {
+
+        return 0;
+    }
+
+    @Override
+    public boolean register(UserModel userModel) {
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(userModel.getUsername());
+        moocUserT.setAddress(userModel.getAddress());
+        moocUserT.setEmail(userModel.getEmail());
+        moocUserT.setUserPhone(userModel.getPhone());
+        String md5Password = MD5Util.encrypt(userModel.getPassword());
+        moocUserT.setUserPwd(md5Password);
+        Integer result = moocUserTMapper.insert(moocUserT);
+        if (result > 0 ) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean checkUsername(String username) {
+        return false;
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        return null;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+        return null;
+    }
+}
