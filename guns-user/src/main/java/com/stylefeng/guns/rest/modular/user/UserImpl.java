0cb5a3cd75dd0891b77c9ce01fe905cbe6229981
liuenci <154910381@qq.com>
2019-09-14 17:19:23 +0800

    :construction: JWT 修改测试和总结

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
@@ -2,14 +2,36 @@ package com.stylefeng.guns.rest.modular.user;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
 import org.springframework.stereotype.Component;
 
 @Component
 @Service(interfaceClass = UserAPI.class)
 public class UserImpl implements UserAPI{
+
     @Override
-    public boolean login(String username, String password) {
-        System.out.println("this is user service!! " + username + " " + password);
+    public int login(String username, String password) {
+        return 3;
+    }
+
+    @Override
+    public boolean register(UserModel userModel) {
         return false;
+    }
+
+    @Override
+    public boolean checkUsername(String username) {
+        return false;
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        return null;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+        return null;
     }
 }
