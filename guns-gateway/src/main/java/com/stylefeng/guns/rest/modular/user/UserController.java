fbe5682256e192f9f6893b39bdae9b7ec42dc398
liuenci <154910381@qq.com>
2019-09-15 23:02:48 +0800

    :construction: 网关模块-用户名检查和退出功能实现

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
@@ -4,6 +4,8 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.api.user.UserAPI;
 import com.stylefeng.guns.api.user.UserModel;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,7 @@ public class UserController {
     @Reference(interfaceClass = UserAPI.class)
     private UserAPI userAPI;
 
-    @RequestMapping("register")
+    @PostMapping("register")
     public ResponseVO register(UserModel userModel){
         if (userModel.getUsername() == null && userModel.getUsername().trim().length() == 0) {
             return ResponseVO.serviceFail("用户名不能为空");
@@ -27,5 +29,36 @@ public class UserController {
         } else {
             return ResponseVO.serviceFail("注册失败");
         }
+    }
+
+    @PostMapping("check")
+    public ResponseVO check(String username){
+        if (username != null && username.trim().length() > 0) {
+            // 当返回 true 的时候，表示用户名可用
+            boolean notExists = userAPI.checkUsername(username);
+            if (notExists) {
+                return ResponseVO.sussess("用户名不存在");
+            } else {
+                return ResponseVO.serviceFail("用户名已存在");
+            }
+        } else {
+            return ResponseVO.serviceFail("用户名不能为空");
+        }
+    }
+
+    @GetMapping("logout")
+    public ResponseVO logout(){
+        /**
+         * 应用:
+         *  1. 前端存储JWT七天：JWT的刷新
+         *  2. 服务器端会存储活动用户信息30分钟
+         *  3. JWT里的userId为key,查找活跃用户
+         * 退出:
+         *  1. 前端删除掉JWT
+         *  2. 后端服务器删除活跃用户缓存
+         * 现状：
+         *  1. 前端删除掉JWT
+         */
+        return ResponseVO.sussess("用户退出成功");
     }
 }
