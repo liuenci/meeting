1559a8c2d837149d6b038455717cd96063e2619a
liuenci <liuencier@gmail.com>
2019-09-19 23:30:58 +0800

    :rocket: 影片Api开发

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/film/FilmServiceApi.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.api.film;
+
+import com.stylefeng.guns.api.film.vo.BannerVo;
+import com.stylefeng.guns.api.film.vo.FilmInfo;
+import com.stylefeng.guns.api.film.vo.FilmVo;
+
+import java.util.List;
+
+public interface FilmServiceApi {
+    BannerVo getBanners();
+    FilmVo getHotFilms(boolean isLimit,int nums);
+    FilmVo getSoonFilms(boolean isLimit, int nums);
+    List<FilmInfo> getBoxRanking();
+    List<FilmInfo> getExpectRanking();
+    List<FilmInfo> getTop();
+}
