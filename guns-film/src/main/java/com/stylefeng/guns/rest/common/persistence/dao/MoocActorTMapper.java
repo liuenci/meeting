b9ac5c67f29e396a8cf29f72f55d38dc75d82082
liuenci <liuencier@gmail.com>
2019-09-20 23:15:09 +0800

    :rocket: 电影模块-数据层生成

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 演员表 Mapper 接口
+ * </p>
+ *
+ * @author liuenci
+ * @since 2019-09-20
+ */
+public interface MoocActorTMapper extends BaseMapper<MoocActorT> {
+
+}
---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFilmTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影片主表 Mapper 接口
+ * </p>
+ *
+ * @author liuenci
+ * @since 2019-09-20
+ */
+public interface MoocFilmTMapper extends BaseMapper<MoocFilmT> {
+
+}
