5ce241c5a9272052620d0ff04858bd1c56c86529
liuenci <154910381@qq.com>
2019-09-22 10:46:24 +0800

    :rocket: 电影模块-API整合

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -1,5 +1,8 @@
 package com.stylefeng.guns.rest.modular.film;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.film.FilmServiceApi;
+import com.stylefeng.guns.rest.modular.film.vo.FilmIndexVo;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/film/")
 public class FilmController {
+    @Reference(interfaceClass = FilmServiceApi.class)
+    private FilmServiceApi filmServiceApi;
 
     @GetMapping("getIndex")
-    public ResponseVO getIndex(){
-        return null;
+    public ResponseVO<FilmIndexVo> getIndex() {
+        FilmIndexVo filmIndexVo = new FilmIndexVo();
+        filmIndexVo.setBanners(filmServiceApi.getBanners());
+        filmIndexVo.setHotFilms(filmServiceApi.getHotFilms(true, 8));
+        filmIndexVo.setSoonFilms(filmServiceApi.getSoonFilms(true, 8));
+        filmIndexVo.setBoxRanking(filmServiceApi.getBoxRanking());
+        filmIndexVo.setExpectRanking(filmServiceApi.getExpectRanking());
+        filmIndexVo.setTop100(filmServiceApi.getTop());
+        return ResponseVO.sussess(filmIndexVo);
     }
 }
