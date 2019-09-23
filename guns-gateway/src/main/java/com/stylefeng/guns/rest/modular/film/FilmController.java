7fcd3c60524dd7f443ac35da78f69ac91392d1f3
liuenci <154910381@qq.com>
2019-09-23 23:05:18 +0800

    :rocket: 条件列表实现-表现层业务实现(1)

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -2,12 +2,17 @@ package com.stylefeng.guns.rest.modular.film;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.api.film.FilmServiceApi;
+import com.stylefeng.guns.api.film.vo.CatVO;
+import com.stylefeng.guns.rest.modular.film.vo.FilmConditionVO;
 import com.stylefeng.guns.rest.modular.film.vo.FilmIndexVo;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/film/")
@@ -32,6 +37,26 @@ public class FilmController {
     public ResponseVO getConditionList(@RequestParam(name = "catId",required = false, defaultValue = "99") String catId,
                                        @RequestParam(name = "sourceId",required = false, defaultValue = "99") String sourceId,
                                        @RequestParam(name = "yearId",required = false, defaultValue = "99") String yearId) {
+        FilmConditionVO filmConditionVO = new FilmConditionVO();
+
+        boolean flag = false;
+        List<CatVO> cats = filmServiceApi.getCats();
+        List<CatVO> catResult = new ArrayList<>();
+        CatVO cat = null;
+        for (CatVO catVo : cats) {
+            if (catVo.getCatId().equals("99")) {
+                cat = catVo;
+                continue;
+            }
+            if (cat.getCatId().equals(catId)) {
+                flag = true;
+                cat.setActive(true);
+            }
+            if (!flag) {
+                cat.setActive(true);
+                catResult.add(cat);
+            }
+        }
         return null;
     }
 }
