6c8ea031ca881e4f8baa68ea9588178d994d0bd7
liuenci <liuencier@gmail.com>
2020-02-19 14:56:58 +0800

    :sparkles:业务系统集成Dubbo异步调用（1）

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/GateWayApplication.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/GateWayApplication.java
@@ -3,7 +3,9 @@ package com.stylefeng.guns.rest;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 @EnableDubboConfiguration
 public class GateWayApplication {
