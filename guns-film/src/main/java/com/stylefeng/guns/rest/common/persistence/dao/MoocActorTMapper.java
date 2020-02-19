2fdf95b06566ccad9bc97e48988c517235e1d0ad
liuenci <liuenci@cvte.com>
2020-02-19 10:39:54 +0800

    :sparkles:影片详情查询第二部分-数据层实现（2）

--- guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocActorTMapper.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocActorTMapper.java
@@ -1,7 +1,11 @@
 package com.stylefeng.guns.rest.common.persistence.dao;
 
+import com.stylefeng.guns.api.film.vo.ActorVO;
 import com.stylefeng.guns.rest.common.persistence.model.MoocActorT;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +16,7 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  * @since 2019-09-20
  */
 public interface MoocActorTMapper extends BaseMapper<MoocActorT> {
+
+    List<ActorVO> getActors(@Param("filmId") String filmId);
 
 }
