d2b4f2700d87b9f95454c37373a19e9bcc2348b2
liuenci <liuencier@gmail.com>
2020-08-03 20:25:12 +0800

    :sparkles: 订单模块-orderController

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
@@ -0,0 +1,18 @@
+package com.stylefeng.guns.rest.modular.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: meeting
+ * @description: 订单控制器
+ * @author: liuenci
+ * @create: 2020-08-03 20:11
+ **/
+@Slf4j
+@RestController
+@RequestMapping("/order/")
+public class OrderController {
+
+}
