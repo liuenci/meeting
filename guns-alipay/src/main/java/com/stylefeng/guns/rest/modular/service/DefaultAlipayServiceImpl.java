cedb848794e1860f75d2eb18c949aaf5f34b456a
liuenci <liuencier@gmail.com>
2020-08-18 10:38:01 +0800

    :sparkles: ali当面付服务静态创建

--- guns-alipay/src/main/java/com/stylefeng/guns/rest/modular/service/DefaultAlipayServiceImpl.java
+++ guns-alipay/src/main/java/com/stylefeng/guns/rest/modular/service/DefaultAlipayServiceImpl.java
@@ -7,6 +7,12 @@ import com.stylefeng.guns.api.alipay.vo.AliPayInfoVO;
 import com.stylefeng.guns.api.alipay.vo.AliPayResultVO;
 import com.stylefeng.guns.api.order.OrderServiceAPI;
 import com.stylefeng.guns.core.util.FTPUtils;
+import com.stylefeng.guns.rest.modular.alipay.config.Configs;
+import com.stylefeng.guns.rest.modular.alipay.service.AlipayMonitorService;
+import com.stylefeng.guns.rest.modular.alipay.service.AlipayTradeService;
+import com.stylefeng.guns.rest.modular.alipay.service.impl.AlipayMonitorServiceImpl;
+import com.stylefeng.guns.rest.modular.alipay.service.impl.AlipayTradeServiceImpl;
+import com.stylefeng.guns.rest.modular.alipay.service.impl.AlipayTradeWithHBServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -28,7 +34,35 @@ public class DefaultAlipayServiceImpl implements AliPayServiceAPI {
     @Autowired
     private FTPUtils ftpUtils;
 
-//    private static AlipayTradeService tradeService;
+    // 支付宝当面付2.0服务
+    private static AlipayTradeService tradeService;
+
+    // 支付宝当面付2.0服务（集成了交易保障接口逻辑）
+    private static AlipayTradeService tradeWithHBService;
+
+    // 支付宝交易保障接口服务，供测试接口API使用，有一份 readme.txt 可供使用
+    private static AlipayMonitorService monitorService;
+
+    static {
+        // 一定要在创建AlipayTradeService之前调用Config.init()设置默认参数
+        // Configs会读取classpath下的zfbinfo.properties文件配置信息,如果找不到该文件则确认该文件是否在classpath目录
+        Configs.init("zfbinfo.properties");
+
+        // 使用Configs提供的默认参数
+        // AlipayTradeService 可以使用单例或者静态成员对象，不需要反复new
+        tradeService = new AlipayTradeServiceImpl.ClientBuilder().build();
+
+        // 支付宝当面付2.0服务（集成了交易保障接口逻辑）
+        tradeWithHBService = new AlipayTradeWithHBServiceImpl.ClientBuilder().build();
+
+        // 如果需要在程序中覆盖Configs提供的默认参数,可以使用ClientBuilder类的setXXX方法修改默认参数 否则使用代码中的默认设置
+        monitorService = new AlipayMonitorServiceImpl.ClientBuilder()
+                .setGatewayUrl("http://mcloudmonitor.com/gateway.do")
+                .setCharset("GBK")
+                .setFormat("json")
+                .build();
+    }
+
     @Override
     public AliPayInfoVO getQRCode(String orderId) {
         return null;
