830c0fb06fa056f260c2cb64758d31bf68913eef
“liuenci” <liuencier@gmail.com>
2020-08-26 08:54:05 +0800

    :sparkles: 增加网关启动日志输出

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/GateWayApplication.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/GateWayApplication.java
@@ -1,6 +1,7 @@
 package com.stylefeng.guns.rest;
 
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -8,6 +9,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@Slf4j
 @EnableAsync
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 @EnableDubboConfiguration
@@ -18,6 +20,6 @@ public class GateWayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
-
+        log.info("========网关启动成功========");
     }
 }
